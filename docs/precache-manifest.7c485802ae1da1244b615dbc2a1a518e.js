51384ea1d816177739727b6acb0b1edeede27ad2
TonyStr <tony.stroemsnaes@gmail.com>
2019-01-16 21:14:46 +0100

    added public build

---
+++ docs/precache-manifest.7c485802ae1da1244b615dbc2a1a518e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "33902ee99a169654235a",
+    "url": "/static/js/main.33902ee9.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "33902ee99a169654235a",
+    "url": "/static/css/main.3b88fd42.chunk.css"
+  },
+  {
+    "revision": "008553fd438ae07ca2adf7a0daaad3bb",
+    "url": "/index.html"
+  }
+];
