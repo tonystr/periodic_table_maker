92e4cd79e690bbd69fbb4d473db524d5754f12ad
Tony <tony.stroemsnaes@gmail.com>
2019-02-06 11:52:38 +0100

    many progress

---
+++ server/server.js
@@ -0,0 +1,114 @@
+
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const app = express();
+const port = 5487;
+
+const config = {
+    user: 'foo',
+    password: 'bar',
+    host: '127.0.0.1',
+    port: 3306,
+    database: 'periodic_table'
+}
+
+const con = mysql.createConnection(config);
+    
+app.use(cors());
+
+app.get('/', (servReq, servRes) => {
+
+    console.log(`got request: ${servReq.headers.data}`);
+
+    const data = JSON.parse(servReq.headers.data);
+
+    if (data && data.reqtype) {
+
+        con.connect(err => {
+            if (err) return console.error(err);
+
+            switch (data.method) {
+                case 'GET':
+                    console.log('table reqtype: ' + data.reqtype);
+                    if (data.reqtype === 'element') {
+                        if (data.anom === undefined) {
+
+                            console.log('anom is undefined');
+                            servRes.send({error: 'proper data not set'});
+                        } else {
+                            con.query(
+                                `SELECT * FROM element WHERE table_id=${data.table} AND atom_number=${data.anom}`,
+                                (err, res) => {
+
+                                if (err) return console.error(err);
+                                console.log('responding with:', res);
+                                servRes.send(res);
+                            });
+                        }
+                    } else if (data.reqtype === 'table_elements') {
+                        console.log('got here');
+                        con.query(
+                            `SELECT atom_number, symbol FROM element WHERE table_id=${data.table}`,
+                            (err, res) => {
+
+                            if (err) return console.error(err);
+                            console.log('responding with:', res);
+                            servRes.send(res);
+                        });
+                    }
+                    break;
+
+                case 'UPDATE':
+                    if (!data.name || !data.symbol || !data.anom) {
+                        servRes.send({error: 'proper data not set'});
+                        return;
+                    }
+                    // UPDATE `periodic_table`.`element` SET `name` = 'Hydrogenium' WHERE (`element_id` = '1');
+                    console.log(`UPDATE element SET
+                        name="${data.name}", symbol="${data.symbol}"
+                        WHERE table_id=${data.table} AND atom_number=${data.anom}`);
+                    con.query(
+                        `UPDATE element SET
+                        name="${data.name}", symbol="${data.symbol}"
+                        WHERE table_id=${data.table} AND atom_number=${data.anom}`,
+                        (err, res) => {
+
+                        if (err) return console.error(err);
+                        console.log('responding with:', res);
+                        servRes.send(res);
+                    });
+                    break;
+
+                case 'ADD':
+                    if (!data.name || !data.symbol || !data.anom) {
+                        const res = { error: 'proper data not set' };
+                        servRes.send(res);
+                        console.log('responding with:', res);
+                        return;
+                    }
+                    con.query(
+                        `INSERT INTO element (name, symbol, atom_number, atomic_mass, electron_negativity, table_id, type_id)
+                        VALUES ("${data.name}", "${data.symbol}", ${data.anom}, 1, 1, 1, 1)`,
+                        (err, res) => {
+
+                        if (err) return console.error(err);
+                        console.log('responding with:', res);
+                        servRes.send(res);
+                    });
+                    //servRes.send('something');
+                    break
+
+                default:
+                    servRes.send({
+                        error: 'Specified data method does not exist'
+                    });
+                    break;
+            }
+        });
+    }
+});
+
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
--- src/pages/table.js
+++ src/pages/table.js
@@ -36,13 +36,15 @@ class Element extends Component {
         super(props);
         this.state = {
             click: props.click || false,
-            elm: props.elm || false
+            elm: props.elm || false,
+            isUndef: this.props.elm && this.props.elm.isDefault
         };
         this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick() {
+    handleClick = () => {
         // this.setState({ click: !this.state.click });
+        console.log(this.state.elm);
         this.props.onSelect(this.state.elm);
     }
 
@@ -50,7 +52,12 @@ class Element extends Component {
         return (
             <div
                 symbol={this.state.elm ? this.state.elm.symbol : undefined}
-                className={'element' + (!this.state.elm ? ' hidden' : '') + (this.state.click ? ' click' : '')}
+                className={
+                    'element' +
+                    (!this.state.elm ? ' hidden' : '') +
+                    (this.state.click ? ' click' : '') +
+                    (this.state.isUndef ? ' undef' : '')
+                }
                 onClick={this.handleClick}
             >
                 {this.state.elm ? this.state.elm.symbol : undefined}
@@ -60,19 +67,80 @@ class Element extends Component {
 }
 
 class Ptable extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loaded: false,
+            elements: [],
+            width: 18,
+            table: 1,
+            reload: this.loadTable
+        };
+
+        this.loadTable();
+    }
+
+    loadTable = () => {
+        this.setState({
+            loaded: false
+        });
+        apiFetch({
+            method: 'GET',
+            reqtype: 'table_elements',
+            table: this.state.table
+        }, res => {
+            console.log('loaded table:', res);
+            this.setState({
+                loaded: true,
+                elements: res
+            });
+        });
+    }
+
     renderGroup(index, width) {
         const list = [];
-        const start = index * width;
+        const start = index * this.state.width;
+
+        const def = {
+            isDefault: true,
+            name: '',
+            symbol: '',
+            atom_number: undefined
+        };
 
         for (let i = 0; i < width; i++) {
             const key = start + i;
-            list.push(
-                <Element
-                    onSelect={this.props.onInspect}
-                    key={key}
-                    elm={elements[key] || undefined}
-                />
-            );
+            let anum = key + 1 -
+                (key > 1  && 16) -
+                (key > 20 && 10) -
+                (key > 38 && 10) +
+                (key > 91 && 14) +
+                (key > 110 && 14);
+
+            let elm = def;
+
+            if ((key > 0  && key < 17) ||
+                (key > 19 && key < 30) ||
+                (key > 37 && key < 48)) {
+
+                elm = Object.assign({}, def, { atom_number: anum, hidden: true });
+            } else if (key === 92 || key === 110) {
+                elm = Object.assign({}, def, { symbol: '+' });
+            } else {
+                elm = (this.state.elements.find(elm => elm.atom_number === anum) ||
+                    Object.assign({}, def, { atom_number: anum, symbol: anum }));
+            }
+
+            if (elm !== undefined) {
+                list.push(
+                    <Element
+                        onSelect={this.props.onInspect}
+                        key={key}
+                        elm={!elm.hidden && elm}
+                    />
+                );
+            }
         }
 
         return (<div className='group' key={index}>{list}</div>);
@@ -83,9 +151,16 @@ class Ptable extends Component {
         const height = 7;
         const list = [];
 
-        for (let i = 0; i < height; i++) {
-            list.push(this.renderGroup(i, width));
+        console.log('rerendering ptable');
+
+        if (this.state.loaded) {
+            for (let i = 0; i < height; i++) {
+                list.push(this.renderGroup(i, width));
+            }
         }
+
+        console.log('state elms: ');
+        console.log(this.state.elements);
 
         return (<div className='ptable'>{list}</div>);
     }
@@ -95,13 +170,126 @@ function XButton(props) {
     return (<div className='button-x' onClick={props.onClick} />);
 }
 
+async function apiFetch(data, callBack) {
+    await fetch('http://localhost:5487', {
+        headers: {
+            accepts: 'application/json',
+            data: JSON.stringify(data)
+        },
+    })
+    .then(res => {
+        res.json().then(json => callBack(json));
+
+    }).catch(e => console.log(e));
+}
+
+function ElementInput(props) {
+    return (
+        <input
+            type='text'
+            defaultValue={props.elm[props.name]}
+            name={props.name}
+            onChange={props.onChange}
+        />
+    );
+}
+
 class ElementCard extends Component {
     constructor(props) {
         super(props);
-        this.handleClick = this.handleClick.bind(this);
+        const undef = !!props.elm.isDefault;
+        console.log('undef:', undef);
+        this.state = {
+            dismount: this.props.inspectorDismount,
+            loaded: undef,
+            isUndef: undef,
+            method: undef ? 'ADD' : 'not sure',
+            changed: false,
+            elm: {
+                symbol: undef ? '' : null,
+                anom: props.elm.atom_number,
+                name: undef ? '' : null,
+                amass: null,
+                eneg: null
+            }
+        }
+        if (!undef) this.fetchElement(this.props.elm.atom_number);
     }
 
-    handleClick(evt) {
+    inputOnChange = (e) => {
+        let elm = this.state.elm;
+        elm[e.target.name] = e.target.value;
+        this.setState({ elm: elm, changed: true });
+    }
+
+    handleDismount = () => {
+        // this.dismount(); is done by parent
+        this.state.dismount();
+    }
+
+    dismount = () => {
+        if (this.state.changed) {
+            // update SQL
+            const elm = this.state.elm;
+            apiFetch({
+                method: this.state.method,
+                reqtype: 'element',
+                table: 1,
+                symbol: elm.symbol,
+                anom: elm.anom,
+                name: elm.name,
+                amass: elm.amass,
+                eneg: elm.eneg
+            }, res => {
+                console.log(res);
+            });
+        } else {
+            console.log('Did not apiFetch, because elm has not changed');
+        }
+    }
+
+    fetchElement = (anom) => {
+
+        console.log('fetch element:', anom);
+
+        const data = {
+            method: 'GET',
+            reqtype: 'element',
+            table: 1, // 'table_test'
+            anom: anom
+        };
+
+        apiFetch(data, res => {
+            if (res.length) {
+                const elm = res[0];
+                this.setState({
+                    loaded: true,
+                    method: 'UPDATE',
+                    elm: {
+                        symbol: elm.symbol,
+                        anom: anom,
+                        name: elm.name
+                    }
+                });
+            } else {
+                this.setState({
+                    loaded: true,
+                    method: 'ADD',
+                    elm: {
+                        symbol: '',
+                        anom: anom,
+                        name: ''
+                    }
+                });
+            }
+        });
+    }
+
+    changeElement = (key, value) => {
+        this.elm[key] = value
+    }
+
+    handleClick = (evt) => {
         evt.stopPropagation();
         evt.nativeEvent.stopImmediatePropagation();
     }
@@ -114,12 +302,21 @@ class ElementCard extends Component {
                         className={'card' + (this.props.dismounting ? ' fadeout' : '')}
                         onClick={this.handleClick}
                     >
-                        <div className='datalist'>
-                            <input type='text' value={this.props.elm.symbol} className='elm-symbol' />
-                            <input type='text' value={this.props.elm.anom}   className='elm-anom' />
-                            <input type='text' value={this.props.elm.name}   className='elm-name' />
-                        </div>
-                        <XButton onClick={this.props.inspectorDismount} />
+                        {this.state.loaded && (
+                            <div className="data-input">
+                                <ul>
+                                    <li>Symbol:</li>
+                                    <li>Atomic Number:</li>
+                                    <li>Name:</li>
+                                </ul>
+                                <form className='datalist' method='POST'>
+                                    <ElementInput name="symbol" elm={this.state.elm} onChange={this.inputOnChange} className='elm-symbol' />
+                                    <ElementInput name="anom"   elm={this.state.elm} onChange={this.inputOnChange} className='elm-anom' />
+                                    <ElementInput name="name"   elm={this.state.elm} onChange={this.inputOnChange} className='elm-name' />
+                                </form>
+                            </div>
+                        )}
+                        <XButton onClick={this.handleDismount} />
                     </div>
                 </div>
             </Center>
@@ -130,14 +327,14 @@ class ElementCard extends Component {
 class Inspector extends Component {
     constructor(props) {
         super(props);
-        this.dismount = this.dismount.bind(this);
         this.state = { dismounting: false };
         this.props.blur(true);
     }
 
-    dismount() {
+    dismount = () => {
         if (this.state.dismounting) return;
         this.setState({ dismounting: true });
+        this.refs.card.dismount();
         this.props.blur(false);
         setTimeout(() => {
             this.props.onDismount();
@@ -154,6 +351,7 @@ class Inspector extends Component {
                     dismounting={this.state.dismounting}
                     inspectorDismount={this.dismount}
                     elm={this.props.elm}
+                    ref='card'
                 />
             </div>
         );
@@ -179,6 +377,7 @@ export default class App extends Component {
 
     onInspectEscape() {
         this.setState({ inspect: false });
+        this.refs.table.state.reload();
     }
 
     render() {
@@ -186,7 +385,7 @@ export default class App extends Component {
             <div className='app'>
                 <div className={this.state.blur ? 'content blur' : 'content'}>
                     <div className='title'> Periodic Table Builder </div>
-                    <Center><Ptable onInspect={this.onInspectElement} /></Center>
+                    <Center><Ptable ref="table" onInspect={this.onInspectElement} /></Center>
                 </div>
                 {this.state.inspect && <Inspector
                     elm={this.state.inspect}
